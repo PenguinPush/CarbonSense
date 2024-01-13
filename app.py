d3e1fe91fd27043405f8e293a6efa5c774014bb5
PenguinPush <icyzminecraft@gmail.com>
2024-01-13 13:11:40 -0500

    fafdsghj

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, url_for, render_template, redirect
-from views import views
 
 app = Flask(__name__)
-app.register_blueprint(views)
+
+shopping_list = []
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/transit')
+def transit():
+    return render_template('transit.html')
+
+
+@app.route('/shopping')
+def shopping():
+    if request.method == 'POST':
+        item = request.form.get('item')
+        quantity = int(request.form.get('quantity'))
+
+        # Append the item and quantity to the shopping list
+        shopping_list.append({'item': item, 'quantity': quantity})
+
+    return render_template('shopping.html', shopping_list=shopping_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
