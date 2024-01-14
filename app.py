2d63733b3472be46b98fff20f922b1a9cbc6bb34
PenguinPush <icyzminecraft@gmail.com>
2024-01-14 10:14:40 -0500

    dfghjkl

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 import json
 import secrets
 import random
+
 client = OpenAI(api_key='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')
 google_maps_api_key = 'AIzaSyCMPdLpbgwvxDXT02Fwk8mqHfNPqop6rxk'
 
@@ -14,6 +15,7 @@ google_maps_api_key = 'AIzaSyCMPdLpbgwvxDXT02Fwk8mqHfNPqop6rxk'
 # )
 
 carbon_counter = client.beta.assistants.retrieve(assistant_id='asst_qiKqmLTo9D8iwgCdOYezPno4')
+random_generator = client.beta.assistants.retrieve(assistant_id='asst_dFjgZHUgleeu3fexaIQVTcVA')
 thread = client.beta.threads.create()
 
 app = Flask(__name__)
@@ -36,56 +38,59 @@ history = [
      'trivia': 'Fun fact, the total carbon emissions of 5 pizza slices is roughly the same as charging 363 smartphones!'}
 ]
 
+
 def calculate_distance_and_route(api_key, origin, destination):
-        base_url = "https://maps.googleapis.com/maps/api/directions/json"
-        params = {
-            'origin': origin,
-            'destination': destination,
-            'key': api_key,
-        }
+    base_url = "https://maps.googleapis.com/maps/api/directions/json"
+    params = {
+        'origin': origin,
+        'destination': destination,
+        'key': api_key,
+    }
 
-        response = requests.get(base_url, params=params)
-        data = response.json()
+    response = requests.get(base_url, params=params)
+    data = response.json()
 
-        if data['status'] == 'OK':
-            # Extract distance and route information
-            distance = data['routes'][0]['legs'][0]['distance']['text']
-            #route = data['routes'][0]['overview_polyline']['points']
-            return distance
-        else:
-            return None
+    if data['status'] == 'OK':
+        # Extract distance and route information
+        distance = data['routes'][0]['legs'][0]['distance']['text']
+        # route = data['routes'][0]['overview_polyline']['points']
+        return distance
+    else:
+        return None
+
 
 def estimate_carbon_emissions(distance):
-        distance_num = ''.join(char for char in distance if char.isdigit())
-        spent_carbon = int(distance_num) * 0.206
-        return round(spent_carbon, 2)
+    distance_num = ''.join(char for char in distance if char.isdigit())
+    spent_carbon = int(distance_num) * 0.206
+    return round(spent_carbon, 2)
+
 
 @app.route('/calculate')
 def calculate():
-     global distance
-     global route
-     global transit_emissions
-     distance = calculate_distance_and_route(google_maps_api_key, origin, destination)
-     transit_emissions = estimate_carbon_emissions(distance)
-     return(redirect('/transit'))
+    global distance
+    global route
+    global transit_emissions
+    distance = calculate_distance_and_route(google_maps_api_key, origin, destination)
+    transit_emissions = estimate_carbon_emissions(distance)
+    return (redirect('/transit'))
+
 
 @app.route('/transit', methods=['GET', 'POST'])
 def transit():
-
     if request.method == 'POST':
-         global origin
-         global destination
-         origin = request.form.get('origin')
-         destination = request.form.get('destination')
-         return redirect('/calculate')
+        global origin
+        global destination
+        origin = request.form.get('origin')
+        destination = request.form.get('destination')
+        return redirect('/calculate')
 
     try:
         return render_template('transit.html', distance=distance, transit_emissions=transit_emissions)
     except:
         return render_template('transit.html')
 
-        #origin_location = input("Enter the origin location: ")
-        #destination_location = input("Enter the destination location: ")
+        # origin_location = input("Enter the origin location: ")
+        # destination_location = input("Enter the destination location: ")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -133,45 +138,47 @@ def get_json():
 
             return redirect('/')
 
-percent_proximity = 0.8
-max_range = 10
 
 def generate_game():
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=""
+    )
 
-    percent_proximity = 0.8
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=random_generator.id
+    )
 
-    list_of_objects = ["plastic water bottle(s)", "plastic bag(s)", "world domination(s)"]
-    list_of_emissions = [3, 4, 5]  # in kg 
+    while True:
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
 
-    object_one_index = random.randint(0, len(list_of_objects)-1)
-    object_two_index = random.randint(0, len(list_of_objects)-1)
+        if run.status == "completed":
+            messages = client.beta.threads.messages.list(
+                thread_id=thread.id
+            )
 
-    amt_of_object_one = random.randint(1, max_range)
-    approx_total_object_two_emissions = None
-    # do the fancy chat gpt thing and get both object 1 and 2 emissions (should be per unit)
-    object_one_emissions = list_of_emissions[object_one_index]
-    object_two_emissions = list_of_emissions[object_two_index]
+            randomitems = str(messages.data[0].content[0].text.value)
+            randomitems = json.loads(randomitems)
 
-    if object_two_emissions > object_one_emissions:
-        object_one_emissions, object_two_emissions = object_two_emissions, object_one_emissions
-        object_one_index, object_two_index = object_two_index, object_one_index
+            randomitems1 = randomitems[0]
+            randomitems2 = randomitems[1]
 
-    total_object_one_emissions = object_one_emissions * amt_of_object_one
+            if randomitems1['total_carbon_emissions_kg'] > randomitems2['total_carbon_emissions_kg']:
+                answer = True
+            else:
+                answer = False
 
-    approx_total_object_two_emissions = object_one_emissions + (random.uniform(-1, 1) * percent_proximity * object_one_emissions)
+            print([randomitems1['item'], randomitems1['quantity'], randomitems2['item'], randomitems2['quantity'],
+                    randomitems1['total_carbon_emissions_kg'], randomitems2['total_carbon_emissions_kg'], answer])
 
-    amt_of_object_two = approx_total_object_two_emissions // object_two_emissions
-    amt_of_object_two = int(amt_of_object_two)
-    if amt_of_object_two == 0:
-         amt_of_object_two == 1
-    total_object_two_emissions = amt_of_object_two * object_two_emissions
+            return [randomitems1['item'], randomitems1['quantity'], randomitems2['item'], randomitems2['quantity'],
+                    randomitems1['total_carbon_emissions_kg'], randomitems2['total_carbon_emissions_kg'], answer]
 
-    if total_object_one_emissions > total_object_two_emissions:
-        answer = "one"
-    else:
-         answer = "two"
-    
-    return [list_of_objects[object_one_index], amt_of_object_one, list_of_objects[object_two_index], amt_of_object_two, total_object_one_emissions, total_object_two_emissions, answer]
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
@@ -182,15 +189,17 @@ def game():
         answerTwo = request.form.get("two")
         print(answerOne)
         print(answerTwo)
-        if answerOne is not None and answer=="one":
-             result = "Correct"
-        if answerTwo is not None and answer=="two":
-             result = "Correct"
-        if answerOne is not None and answer=="two":
-             result = "Wrong"
+        if answerOne is not None and answer == True:
+            result = "Correct"
+        if answerTwo is not None and answer == False:
+            result = "Correct"
+        if answerOne is not None and answer == False:
+            result = "Wrong"
         else:
-             result = "Wrong"
-    return render_template('game.html', object_one=obj_one, amt_of_one=num_obj_one, object_two=obj_two, amt_of_two=num_obj_two, total_one=total_one, total_two=total_two, result=result)
+            result = "Wrong"
+    return render_template('game.html', object_one=obj_one, amt_of_one=num_obj_one, object_two=obj_two,
+                           amt_of_two=num_obj_two, total_one=total_one, total_two=total_two, result=result)
+
 
 @app.route('/about')
 def about():
