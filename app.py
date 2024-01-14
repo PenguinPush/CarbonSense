1489c0575c516e9c3aff878b0fbcc42695aa7b16
PenguinPush <icyzminecraft@gmail.com>
2024-01-14 04:04:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,15 @@ thread = client.beta.threads.create()
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(16)
 
-history = []
+history = [
+    {'item': 'Industrial Revolution', 'quantity': 1, 'individual_carbon_emissions_kg': 200000000000, 'total_carbon_emissions_kg': 200000000000, 'trivia': 'The carbon emissions during the Industrial Revolution paved the way for the modern anthropogenic climate change.'},
+    {'item': 'Taylor Swift Concert', 'quantity': 1, 'individual_carbon_emissions_kg': 27500, 'total_carbon_emissions_kg': 27500, 'trivia': 'The carbon emissions from a Taylor Swift Concert equates to flying a plane around the world over 5 times!'},
+    {'item': 'Water Bottles', 'quantity': 24, 'individual_carbon_emissions_kg': 0.082, 'total_carbon_emissions_kg': 1.968, 'trivia': 'The carbon emissions from producing 24 water bottles could fill nearly 237 basketballs!'},
+    {'item': 'Charging Smartphone', 'quantity': 1, 'individual_carbon_emissions_kg': 0.005, 'total_carbon_emissions_kg': 0.005, 'trivia': 'Did you know? The carbon emissions from charging a smartphone are equivalent to driving a regular car for about 15 meters.'},
+    {'item': 'Pizza Slice', 'quantity': 5, 'individual_carbon_emissions_kg': 0.36, 'total_carbon_emissions_kg': 1.8,
+     'trivia': 'Fun fact, the total carbon emissions of 5 pizza slices is roughly the same as charging 363 smartphones!'}
+]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
